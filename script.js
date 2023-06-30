a2cdbd9aed84f8e4359e8b8e81d981c49e9a04e3
avanich28 <arpawan.v@gmail.com>
2023-06-30 10:32:37 +0700

    add responsive

--- script.js
+++ script.js
@@ -2,8 +2,14 @@
 
 const root = document.documentElement;
 const mode = document.querySelector(".mdi-theme-light-dark");
+const starBtn = document.querySelectorAll(".mdi-star-outline");
+const status = document.querySelectorAll(".status");
 
 mode.addEventListener("click", () => {
   root.classList.toggle("dark");
   root.classList.toggle("light");
 });
+
+starBtn.forEach((x, i) =>
+  x.addEventListener("click", () => status[i].classList.toggle("favourite"))
+);
